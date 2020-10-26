00dbc9cf1b946729e4c9f53949a12d3b3d377141
Elliott <elliottrecord@gmail.com>
2020-10-26 21:26:11 +0000

    Added start script

---
+++ start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ -e ./mysql/.env.example ] && [ ! -e ./mysql/.env ];
+then
+  cp ./mysql/.env.example ./mysql/.env
+fi
+
+if [ -e ./strapi/.env.example ] && [ ! -e ./strapi/.env ];
+then
+  cp ./strapi/.env.example ./strapi/.env
+fi
+
+docker-compose restart $1
