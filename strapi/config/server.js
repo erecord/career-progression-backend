4109393f2b24b78a5807cc2d597bd6ee18cb13a3
Elliott <elliottrecord@gmail.com>
2020-10-26 21:20:14 +0000

    Installed Strapi and MySQL in docker-compose environment

---
+++ strapi/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '28236e5b46d4a592701928f32cec6df5'),
+    },
+  },
+});
---
+++ strapi/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'e4ffcd4e-f9df-4557-8edf-87fb3ff782d6'
+};
